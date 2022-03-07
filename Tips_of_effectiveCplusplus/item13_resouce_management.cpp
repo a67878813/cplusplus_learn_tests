66be81c4daefbcd467bbbbbc20abceef8a9bb388
a67878813 <len888len888@gmail.com>
2022-03-07 13:02:36 +0800

    1

--- Tips_of_effectiveCplusplus/item13_resouce_management.cpp
+++ Tips_of_effectiveCplusplus/item13_resouce_management.cpp
@@ -40,38 +40,66 @@ void f2(){
                         // there is only () value Initializer
                         // operator= has not been assigned/overloaded.
 
-}
+    //auto_ptr will auto deleted pInv via destructor when f2 is ended.
+};
 // std::auto_ptr<Investment> pInv(createInvestment());
 
 
+void f3(){
+    //shared_ptr is reference-counting smart pointer,RCSP
+    std::shared_ptr<Investment> pInv( createInvestment() )  ;
+    std::shared_ptr<Investment>
+        pInv1(createInvestment());
+    //std::unique_pointer
+    std::shared_ptr<Investment> pInv2(pInv1); 
+
+    pInv1= pInv2;   
+};
+void f4(){
+    std::shared_ptr<std::string>
+        aps(new std::string[10]);    //ptr is  doing "delete" ,there are memory leak whih this example.
+        //  c-type array
+        //std::string[10] should use delete[] .
+    std::shared_ptr<int> spi(new int[1024]); //same error
+
+};
+
+void f5(){
+    std::shared_ptr<std::vector<std::string>>
+        tt_ptr(new std::vector<std::string>(10) );
+
+    std::shared_ptr<std::vector<int>>
+        tt2_ptr(new std::vector<int>(1024) );
+};
+
 
 int main(){
-std::auto_ptr<Investment> pMYTest(new Investment )  ;
-                        // () initializer
+    std::auto_ptr<Investment> pMYTest(new Investment )  ;
+                            // () initializer
+
+ 
+
+    int  here_is_function(int);
+    int  here_is_also_function(int*);
+    int  here_is_also_function_too(int&);
+    int here_is_also_function (const int& i);
+
+    int here_is_v2(int(5));
+    int here_is_v3(5);
+    // () as varible initilizer is easy to confuse 
+        //when that one is not familier with names or that there are type of pointers.
 
 
+    int  (*here_is_func_pointer___varible)(int);
+    int* here_is_value_pointr2 = new int();
+                        // =  int   (   (int)0  )
 
-int  here_is_function(int);
-int  here_is_also_function(int*);
-int  here_is_also_function_too(int&);
-int here_is_also_function (const int& i);
+    int* here_is_v1_1 = (int*)int(5);   //pessimization     //  pessimist is opposite of an optimist
+    int here_is_v1 = int(5);
 
-int here_is_v2(int(5));
-int here_is_v3(5);
-// () as varible initilizer is easy to confuse 
-    //when that one is not familier with names or that there are type of pointers.
-
-
-int  (*here_is_func_pointer___varible)(int);
-int* here_is_value_pointr2 = new int();
-                    // =  int   (   (int)0  )
-
-int* here_is_v1_1 = (int*)int(5);   //pessimization     //  pessimist is opposite of an optimist
-int here_is_v1 = int(5);
-
-int here_is_functioon1();
-int* aaa = new int();
-int here_is_varible_value_initilized_manner();
+    int here_is_functioon1();
+    int* aaa = new int();
+    int here_is_varible_value_initilized_manner();
 
 
-}
+};
