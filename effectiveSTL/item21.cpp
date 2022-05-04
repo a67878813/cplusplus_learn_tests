1e6fe806932095d5aa49065808ffd7dec2e42b14
a67878813 <len888len888@gmail.com>
2022-05-05 01:56:12 +0800

    11

---
+++ effectiveSTL/item21.cpp
@@ -0,0 +1,15 @@
+// equality  
+// equivalence
+
+// comparitive type(compare equivalence)
+// must return false 
+//when have event of same value(equality);
+
+
+//
+
+int main(){
+
+    // t_speed();
+
+}
