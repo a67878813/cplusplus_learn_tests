686bbdc261dae9f8176fd3728fc6647d59397559
a67878813 <len888len888@gmail.com>
2022-10-20 16:09:47 +0800

    12

---
+++ unpv13e-master/intro/header.c
@@ -0,0 +1,10 @@
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include	"../lib/unp.h"
+
+#ifdef __cplusplus
+}
+#endif
---
+++ unpv13e-master/intro/my_test1.cpp
@@ -0,0 +1,20 @@
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include	"../lib/unp.h"
+
+#ifdef __cplusplus
+}
+#endif
+// #include "../lib/error.c"
+
+int
+main(int argc, char **argv)
+{
+printf("dsfsdfsdf\n");
+
+
+
+}
