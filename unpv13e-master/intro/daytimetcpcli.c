4c9d43e5b92fe14f658c334735760d95ecabd1ca
a67878813 <len888len888@gmail.com>
2022-11-07 00:40:52 +0800

    1231

--- unpv13e-master/intro/daytimetcpcli.c
+++ unpv13e-master/intro/daytimetcpcli.c
@@ -9,6 +9,14 @@ extern "C"{
 }
 #endif
 // #include "../lib/error.c"
+// int
+// Socket(int family, int type, int protocol)
+// {
+// 	int n;
+// 	if(( n = socket(family, type, protocol)) <0)
+// 		err_sys("socket error");
+// 	return (n);
+// }
 
 int
 main(int argc, char **argv)
@@ -16,30 +24,33 @@ main(int argc, char **argv)
 	int					sockfd, n;
 	char				recvline[MAXLINE + 1];
 	//ipv4
-	// struct sockaddr_in	servaddr;
+	struct sockaddr_in	servaddr;
 	//ipv6
-	struct sockaddr_in6	servaddr;
+	// struct sockaddr_in6	servaddr;
 
 
 	if (argc != 2)
 		err_quit("usage: a.out <IPaddress>");
 
 
-	// if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+						//socket ( ip protocol family, )
+	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	// if ( (sockfd = socket(9999, SOCK_STREAM, 0)) < 0)
 	//ipv6
-	if ( (sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0)
+	// if ( (sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0)
 		err_sys("socket error");
+	// sockfd = Socket(AF_INET6, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));//set bytes to 0
-	// servaddr.sin_family = AF_INET;
-	servaddr.sin6_family = AF_INET6;
-	// servaddr.sin_port   = htons(13);//to bits type	/* daytime server */
-	servaddr.sin6_port   = htons(13);//to bits type	/* daytime server */
+	servaddr.sin_family = AF_INET;
+	// servaddr.sin6_family = AF_INET6;
+	servaddr.sin_port   = htons(9999);//to bits type	/* daytime server */
+	// servaddr.sin6_port   = htons(13);//to bits type	/* daytime server */
 	// i means Interface
 	// inet_pton  // Presentation to bitNumber 
-	// if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
 	//                     argv[1] 192.168.2.2
-	if (inet_pton(AF_INET6, argv[1], &servaddr.sin6_addr) <= 0)
+	// if (inet_pton(AF_INET6, argv[1], &servaddr.sin6_addr) <= 0)
 
 		err_quit("inet_pton error for %s", argv[1]);
 
@@ -47,8 +58,9 @@ main(int argc, char **argv)
 	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
 		err_sys("connect error");
 
-    
+    unsigned int te_counter = 0;
 	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
+		te_counter +=1;
 		recvline[n] = 0;	/* null terminate */
 		if (fputs(recvline, stdout) == EOF)
 			err_sys("fputs error");
@@ -56,6 +68,7 @@ main(int argc, char **argv)
 	if (n < 0)
 		err_sys("read error");
 
+	printf("te_counter is %d\n",te_counter);
 	exit(0);
 }
 
---
+++ unpv13e-master/intro/daytimetcpsrv.cpp
@@ -0,0 +1,76 @@
+// #include	"unp.h"
+
+#ifdef __cplusplus
+// namespace ex{
+extern "C"{
+#endif
+
+#include	"../lib/unp.h"
+
+#ifdef __cplusplus
+}
+#endif
+// #include "../lib/error.c"
+
+
+
+int
+main(int argc, char **argv)
+{
+	int					listenfd, connfd;
+	struct sockaddr_in	servaddr;
+	char				buff[MAXLINE];
+	time_t				ticks;
+
+	//create a new socket
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	//initialize a serveraddr
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);//hex to number ? 0.0.0.0?
+	servaddr.sin_port        = htons(9999);	/* daytime server */
+
+	//bidn this socket to the serveraddr
+	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+
+	Listen(listenfd, LISTENQ);
+	//max queue is 1024, after that will return refused to cli.
+
+	// infinity loop
+	/* 
+	
+	*/
+	for ( ; ; ) {
+		// accept will sleep this thread, until core to respont.
+		// after TCp 's three way handshake, it returns a 
+			//Connected Descriptor, which could be using to communicate.
+		connfd = Accept(listenfd, (SA *) NULL, NULL);
+
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+		//format str, copy to buff
+        // Write(connfd, buff, strlen(buff));
+		// raw func
+		// if (write(connfd , buff, strlen(buff)) != strlen(buff))
+			// err_sys("write error");
+
+		printf("strlen buff = %d\n",strlen(buff));
+
+		// printf("strlen temp = %d\n",strlen(temp));
+		for ( size_t j = 0; j<strlen(buff);++j)
+		{
+			// temp[0] = buff[0];
+			write(connfd,&buff[j],1);	
+			printf(".");
+		}
+		printf("\n");
+
+		Close(connfd);
+	}
+}
+
+
+// #ifdef __cplusplus
+// }
+// #endif
