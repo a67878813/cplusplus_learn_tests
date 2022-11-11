3ba12e69855c85ddc6618de2b88c9209422ba136
a67878813 <len888len888@gmail.com>
2022-11-11 19:25:13 +0800

    123

--- unpv13e-master/lib/readline.c
+++ unpv13e-master/lib/readline.c
@@ -5,10 +5,11 @@ static int	read_cnt;
 static char	*read_ptr;
 static char	read_buf[MAXLINE];
 
+//read per byte
 static ssize_t
 my_read(int fd, char *ptr)
 {
-
+	//buff empty, 
 	if (read_cnt <= 0) {
 again:
 		if ( (read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
@@ -19,9 +20,11 @@ again:
 			return(0);
 		read_ptr = read_buf;
 	}
-
+	// has read_buf
 	read_cnt--;
-	*ptr = *read_ptr++;
+	*ptr = *read_ptr++;// eqivalent to   * (read_ptr++)   
+						//  get_origen_rvalue , and    read_ptr +1
+	//ptr's rvalue = read_ptr's rvalue ,and read_ptr+1
 	return(1);
 }
 
@@ -51,6 +54,7 @@ readline(int fd, void *vptr, size_t maxlen)
 ssize_t
 readlinebuf(void **vptrptr)
 {
+	//has valid read_buf
 	if (read_cnt)
 		*vptrptr = read_ptr;
 	return(read_cnt);
--- unpv13e-master/lib/sock_cmp_addr.c
+++ unpv13e-master/lib/sock_cmp_addr.c
@@ -13,7 +13,7 @@ sock_cmp_addr(const struct sockaddr *sa1, const struct sockaddr *sa2,
 
 	switch (sa1->sa_family) {
 	case AF_INET: {
-		return(memcmp( &((struct sockaddr_in *) sa1)->sin_addr,
+		return(memcmp( &  ((struct sockaddr_in *) sa1)  ->sin_addr,
 					   &((struct sockaddr_in *) sa2)->sin_addr,
 					   sizeof(struct in_addr)));
 	}
