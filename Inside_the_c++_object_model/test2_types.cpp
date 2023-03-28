748039567ed7862b44b0cad3f2961d812bd98ee1
a67878813 <len888len888@gmail.com>
2023-03-28 12:25:51 +0800

    3

---
+++ Inside_the_c++_object_model/test2_types.cpp
@@ -0,0 +1,53 @@
+#include <memory>
+
+struct AFunctionObj{
+    double operator()(char, int);
+    float operator()(int);
+} f;
+
+template<typename F, typename...Args>
+
+using InvokeResultOfFunc = decltype( F{} (Args{}...));// could be asserted by ctor's fail
+                                    //F{} instantiate a function obj
+                                    //Args{}instantiate some args objs.
+
+//equal to 
+
+template<typename F, typename...Args>
+                        // std::declval< F >() 
+                        // return an F&& type
+using InvokeResultOfFunc2  = decltype(std::declval<F>()  (std::declval<Args>() ... ) );
+                            // decltype ()    // defination type
+                            //F 's define type
+
+
+
+using T31 = InvokeResultOfFunc2<AFunctionObj, char, int>; // double
+using T32 = InvokeResultOfFunc2<AFunctionObj,  int>; // float
+
+template<typename T> T&& declval();
+
+
+template<typename T> struct declval_protector 
+{
+    static constexpr bool value = false;
+};
+template<typename T> T&& declval()
+{
+    static_assert(declval_protector<T>::value, // alert when using wrong
+    // false,//always aleart
+    "   declval only used in dectype/sizeof");
+}
+
+
+
+
+
+
+int main()
+{
+    // declval<AFunctionObj>();
+    return 1;
+
+};
+
